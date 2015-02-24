cbe26985ddd8c7f518128be85f67479ca314c019
Segun Famisa <segunfamisa@gmail.com>
2015-02-24 14:14:46 +0100

    -First commit -Solved all javascripting challenges from nodeschool.io

---
+++ javascripting/function-arguments.js
@@ -0,0 +1,5 @@
+function math(firstArgument, secondArgument, thirdArgument){
+  return (secondArgument * thirdArgument) + firstArgument;
+};
+
+console.log(math(53, 61, 67));
---
+++ javascripting/if-statement.js
@@ -0,0 +1,8 @@
+var fruit = 'orange';
+
+if(fruit.length > 5){
+  console.log('The fruit name has more than five characters.');
+}
+else{
+  console.log('The fruit name has 5 characters or less.');
+}
